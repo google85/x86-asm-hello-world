73aebed9940c3b9b0c3870cb7b3afc397d3c6184
Florin B. <florin.buzec@gmail.com>
2026-02-17 17:51:26 +0200

    update src code

--- src/hello_world.asm
+++ src/hello_world.asm
@@ -3,10 +3,22 @@
 ; Author: Florin Google - based on John Hammond YouTube video
 ; Date: 2026-02-17
 
+global _start
 
 section .text:
-    mov eax, 0x4        ; use the write syscall
 
+_start:
+    mov eax, 0x4            ; use the write syscall
+    mov ebx, 1              ; use stdout as the fd
+    mov ecx, message        ; use the message as the buffer
+    mov edx, message_length ; and supply the length
+    int 0x80                ; invoke the syscall
+
+    ; now gracefully exit
+
+    mov eax, 0x1            ; use the exit syscall
+    mov ebx, 0              ; return code
+    int 0x80                ; invoke the syscall
 
 section .data:
     message: db "Hello World", 0xA
